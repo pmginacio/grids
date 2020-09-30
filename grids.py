489492fc1152153cd929dd1a8b3398088d589b87
Pedro Inácio <pedromiragaia@gmail.com>
2020-09-30 14:11:46 +0200

    added gitignore and improved grids object creation

--- grids.py
+++ grids.py
@@ -3,6 +3,7 @@
 import numpy as np
 import utils
 import cPickle as pickle
+import bz2
 
 __author__ = "Pedro Inácio"
 __copyright__ = "Copyright 2020"
@@ -27,14 +28,17 @@ class Grid(object):
 		super(Grid, self).__init__()
 
 		# get input x and y lists
-		xyz = kwargs.pop('xyz',None)
-		if xyz:
+		xyz = kwargs.pop('xyz', None)
+
+		if xyz is not None:
 			xin = xyz[:,0]
 			yin = xyz[:,1]
 			zin = xyz[:,2]
 
 			# compute lims and spacing
-			xin.sort(), yin.sort()
+			# sort and keep unique nodes
+			xin = np.unique(xin)
+			yin = np.unique(yin)
 			xlimin = [xin[0], xin[-1]]
 			ylimin = [yin[0], yin[-1]]
 
@@ -42,50 +46,65 @@ class Grid(object):
 			dxin = np.min(xin[1:] - xin[0:-1])
 			dyin = np.min(yin[1:] - yin[0:-1])
 
-		# get optional grid parameters and check consistency with xyz input
-		self.xlim = kwargs.pop('xlim', xlimin)
-		if self.xlim[0] > xlimin[0] or self.xlim[1] < xlimin[1]:
-			raise ValueError('xlim bounds are smaller than the input xyz list of points')
+			# get optional grid parameters and check consistency with xyz input
+			self.xlim = kwargs.pop('xlim', xlimin)
+			if self.xlim[0] > xlimin[0] or self.xlim[1] < xlimin[1]:
+				raise ValueError('xlim bounds are smaller than the input xyz list of points')
 
-		self.ylim = kwargs.pop('ylim', ylimin)
-		if self.ylim[0] > ylimin[0] or self.ylim[1] < ylimin[1]:
-			raise ValueError('ylim bounds are smaller than the input xyz list of points')
+			self.ylim = kwargs.pop('ylim', ylimin)
+			if self.ylim[0] > ylimin[0] or self.ylim[1] < ylimin[1]:
+				raise ValueError('ylim bounds %s are smaller than the input xyz list of points %s' % (str(ylimin),str(self.ylim)))
 
-		self.dx = kwargs.pop('dx',dxin)
-		if self.dx > dxin:
-			raise ValueError('dx is larger than minimin spacing in xyz list of points')
+			self.dx = kwargs.pop('dx',dxin)
+			if self.dx > dxin:
+				raise ValueError('dx is larger than minimin spacing in xyz list of points')
 
-		self.dy = kwargs.pop('dy',dyin)
-		if self.dy > dyin:
-			raise ValueError('dy is larger than minimin spacing in xyz list of points')
+			self.dy = kwargs.pop('dy',dyin)
+			if self.dy > dyin:
+				raise ValueError('dy is larger than minimin spacing in xyz list of points')
+
+		else:
+			self.xlim = kwargs.pop('xlim')
+			self.ylim = kwargs.pop('ylim')
+			self.dx = kwargs.pop('dx')
+			self.dy = kwargs.pop('dy')
+			zin = kwargs.pop('z')
 
 		# generate grid coordinates
 		# compute number of grid nodes
 		self.nx = (self.xlim[1] - self.xlim[0]) / float(self.dx) + 1
 		self.ny = (self.ylim[1] - self.ylim[0]) / float(self.dy) + 1
 		# nx should be very close to an integer
-		if not utils.num_equal(self.nx, int(self.nx)):
+		if not np.isclose(self.nx, int(self.nx)):
 			raise ValueError('dx does not divide the xyz list in an integer number of nodes')
-		if not utils.num_equal(self.ny, int(self.ny)):
+		if not np.isclose(self.ny, int(self.ny)):
 			raise ValueError('dy does not divide the xyz list in an integer number of nodes')
+		self.nx = int(self.nx)
+		self.ny = int(self.ny)
 
 		self.x = np.linspace(self.xlim[0],self.xlim[1],self.nx)
 		self.y = np.linspace(self.ylim[0],self.ylim[1],self.ny)
 
 		# check that all xin and yin are present in self.x and self.y, otherwise,
 		# the input list of points is not regularly spaced.
-		if not all([utils.num_equal(np.min(self.x - i), 0.0) for i in xin]):
-			raise ValueError('x list of points is not regularly spaced')
-		if not all([utils.num_equal(np.min(self.y - i), 0.0) for i in yin]):
-			raise ValueError('y list of points is not regularly spaced')
+		if xyz is not None:
+			res = np.array([np.min(np.abs(self.x - i)) for i in xin])
+			if not np.allclose(res, 0.0):
+				raise ValueError('x list of points is not regularly spaced')
+			res = np.array([np.min(np.abs(self.y - i)) for i in yin])
+			if not np.allclose(res, 0.0):
+				raise ValueError('y list of points is not regularly spaced')
 
 		# arrays are stored with y-along the rows and x- along the columns
-		self.z = np.array((self.ny,self.nx),np.nan)
-		# store input list
-		for x,y,z in xyz:
-			ix = int(x-self.xlim[0]/float(dx))
-			iy = int(y-self.ylim[0]/float(dy))
-			self.z[i,j] = z
+		if xyz is not None:
+			self.z = np.full((self.ny,self.nx),np.nan)
+			# store input list
+			for x,y,z in xyz:
+				ix = int((x-self.xlim[0])/float(self.dx))
+				iy = int((y-self.ylim[0])/float(self.dy))
+				self.z[iy,ix] = z
+		else:
+			self.z = zin
 
 		# optional metadata
 		self.xlabel = kwargs.pop('xlabel', None)
@@ -96,28 +115,34 @@ class Grid(object):
 
 		self.zlabel = kwargs.pop('zlabel', None)
 		self.zunit = kwargs.pop('zunit', None)
-	
 
-	# def pack(self):
-
-	# def unpack(z):
+		self.date = kwargs.pop('date', None)
 
 	def save(self, file):
 		'''
 		save the grid to a file using pickle
 		'''
 
-		with open(file, 'wb') as f:
+		with bz2.BZ2File(file, 'wb') as f:
 			#  pickle using the highest protocol available.
 			pickle.dump(self, f, -1)
 
 	def regrid(self, grd):
+		'''
+		interpolate grid data into another grid
+		'''
 
-	def plot(self, file):
+		raise NotImplementedError()
+
+	def plot(self, file, sym_colorbar=False):
 		import matplotlib.pyplot as plt
 
 		fig, ax = plt.subplots()
-		im = ax.imshow(trend, origin='lower', cmap='bwr', vmax=10, vmin=-10)
+		if sym_colorbar:
+			# make symetric colorbar around 0
+			vmax = np.max(np.abs([np.min(self.z), np.max(self.z)]))
+			vmin = -vmax
+		im = ax.imshow(self.z, origin='lower', cmap='bwr', vmin=vmin, vmax=vmax)
 		fig.colorbar(im, ax=ax, orientation='horizontal', fraction=.1)
 		fig.savefig(file)
 
@@ -127,21 +152,24 @@ class Grid(object):
 		'''
 
 		aux = 'Grid:\n'
-		aux += 'Shape:	%dx%d\n' % (self.nx, self.ny)
-		aux += 'Limits:	%f-%f,%f-%f\n' % (self.xlim, self.ylim)
-		aux += 'Spacing:	%f,%f\n' % (self.dx, self.dy)
-		aux += 'Units:	%s, %s, %s\n' % (self.xunit, self.yunit, self.zunit)
-		aux += 'Label:	%s, %s, %s\n' % (self.xlabel, self.ylabel, self.zlabel)
-		aux += 'Data:\n%s\n' % (str(self.data))
+		aux += 'Shape:		%dx%d\n' % (self.nx, self.ny)
+		aux += 'Limits:		' + str(self.xlim) + ', ' + str(self.ylim) + '\n'
+		aux += 'Spacing:	%.2f, %.2f\n' % (self.dx, self.dy)
+		aux += 'Units:		%s, %s, %s\n' % (self.xunit, self.yunit, self.zunit)
+		aux += 'Label:		%s, %s, %s\n' % (self.xlabel, self.ylabel, self.zlabel)
+		aux += 'Date:		%s\n' % (self.date,)
+		aux += 'Data:\n%s\n' % (self.z,)
+
+		return aux
 
 	# implement operators
 	# article that covers the subject
 	#	https://realpython.com/operator-function-overloading/
 
-def read(file):
+def load(file):
 	'''load grid from the pickle file'''
 
-	with open(file, 'rb') as f:
-		grd = pickle.load(file)
+	with bz2.BZ2File(file, 'rb') as f:
+		grd = pickle.load(f)
 
 	return grd
